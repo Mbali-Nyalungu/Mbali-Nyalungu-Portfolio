8f02da6fca910759bebac7ab3a4764a4b2dc6272
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2025-03-14 15:24:12 +0200

    made homepage

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [ {path:'', component: AppComponent},
+{ path: 'projects', component: ProjectsComponent }, 
+];
