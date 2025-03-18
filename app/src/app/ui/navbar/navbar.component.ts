890aa85570f6826061781bb8655aa305127f49d1
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2025-03-18 15:50:30 +0200

    created projects

--- app/src/app/ui/navbar/navbar.component.ts
+++ app/src/app/ui/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
